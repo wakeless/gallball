e7747ee524b6894e4cc5be02f0cc2659302dd882
Michael Gall <michael@wakeless.net>
2012-03-23 11:24:23 +1100

    Adding better validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,8 @@ class Game < ActiveRecord::Base
   belongs_to :winner, :class_name => "Player"
   belongs_to :loser, :class_name => "Player"
 
-  validates_presence_of :winner, :loser
+  validates_presence_of :winner, message: "Even life has a winner"
+  validates_presence_of :loser, message: "Without losers, there can be no winners"
 
   def default_scope
     order("games.date_added desc")
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
 describe Game do
-  it { should validate_presence_of :winner }
-  it { should validate_presence_of :loser }
+  it { should validate_presence_of(:winner).with_message("Even life has a winner") }
+  it { should validate_presence_of(:loser).with_message("Without losers, there can be no winners") }
 end
