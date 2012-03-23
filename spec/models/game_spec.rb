9459a9392cc3e535926cbd87756b511a7ea97cca
Michael Gall <michael@wakeless.net>
2012-03-23 12:13:16 +1100

    Fixing shit

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -4,7 +4,7 @@ describe Game do
   it { should belong_to(:sport) }
   it { should belong_to(:loser).class_name("Player") }
   it { should belong_to(:winner).class_name("Player") } 
-  it { should_validate_presence_of(:sport).with_message("When you play nothing, there is no winner or loser") }
+  it { should validate_presence_of(:sport).with_message("When you play nothing, there is no winner or loser") }
   it { should validate_presence_of(:winner).with_message("Even life has a winner") }
   it { should validate_presence_of(:loser).with_message("Without losers, there can be no winners") }
 end
